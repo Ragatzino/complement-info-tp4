a31e0265a69c88ce143bb47c44920b1587cfb5c0
User <antoinebrunetti@gmail.com>
2019-10-10 00:03:50 +0200

    fix assert tests

--- test/test_compte.py
+++ test/test_compte.py
@@ -5,10 +5,16 @@ compte_service = CompteService()
 compte_dao = CompteDao()
 
 
+""" 
+    assert verifie si la condition est vraie et fait en sorte que le programme s'arrête si la condition n'est pas vraie
+"""
 def test_creer_compte():
     compte = compte_service.creer_compte('TestPseudo', 'MotDePassePseudo')
     if compte.id:
         compte_dao.delete(compte)
+    """ 
+        assert verifie si l'id du compte a été créé
+    """
     assert compte.id and compte.id > 0
 
 
@@ -16,9 +22,21 @@ def test_valid_pseudo_connu():
     compte = compte_service.creer_compte('TestPseudo', 'MotDePassePseudo')
     dispo = compte_service.pseudo_disponible('TestPseudo')
     compte_dao.delete(compte)
+    """ 
+           assert verifie si le compte est effectivement pas dispo
+    """
     assert not(dispo)
 
 
 def test_valid_pseudo_inconnu():
     dispo = compte_service.pseudo_disponible('PseudoFantaisiste')
+    """ 
+               assert verifie si le compte est effectivement dispo
+    """
     assert dispo
+
+if __name__ == "__main__":
+    test_creer_compte()
+    test_valid_pseudo_connu()
+    test_valid_pseudo_inconnu()
+    print("Everything passed")
